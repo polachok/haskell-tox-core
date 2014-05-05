69d7cb339c5e59c6f94051f71fb49ca0c2a069f5
Oliver Hunt <oliver.huntuk@gmail.com>
2014-05-05 20:43:19 +0100

    Added getting of address, doesn't work though

--- Test.hs
+++ Test.hs
@@ -1,5 +1,11 @@
 import Tox.Core
+import Control.Concurrent
 
 main = do
         let tox = toxNew False
+        address <- toxGetAddress tox
+        putStrLn address
+        putStrLn $ show $ toxIsconnected tox
+        toxBootstrapFromAddress tox "192.254.75.98" False 33445 "951C88B7E75C867418ACDB5D273821372BB5BD652740BCDF623A4FA293E75D2F"
+        threadDelay 1000000
         putStrLn $ show $ toxIsconnected tox
--- Tox/Core.hs
+++ Tox/Core.hs
@@ -3,6 +3,11 @@ module Tox.Core where
 import Foreign
 import Foreign.Ptr
 import Foreign.C.Types
+import Foreign.Marshal.Utils
+import Foreign.Marshal.Array
+import Foreign.Marshal.Alloc
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString as BS
 
 -- | Uninspectable data type for the Tox struct
 data Tox
@@ -20,5 +25,33 @@ toxNew ipv6 = case (ipv6) of
 foreign import ccall "tox_isconnected"
     c_tox_isconnected :: Ptr Tox -> CInt
 
-toxIsconnected :: Ptr Tox -> Int
-toxIsconnected tox = fromIntegral $ c_tox_isconnected tox
+toxIsconnected :: Ptr Tox -> Bool
+toxIsconnected tox = (1==) $ fromIntegral $ c_tox_isconnected tox
+
+
+foreign import ccall "tox_bootstrap_from_address"
+    c_tox_bootstrap_from_address :: Ptr Tox -> Ptr CUChar -> CUChar -> CUShort -> Ptr CUChar -> IO ()
+
+toxBootstrapFromAddress :: Ptr Tox -> String -> Bool -> Int -> String -> IO ()
+toxBootstrapFromAddress tox addr ipv6 port key = do
+        k <- newArray $ map (CUChar . fromIntegral . fromEnum) key
+        a <- newArray $ map (CUChar . fromIntegral . fromEnum) addr
+        let p = CUShort . fromIntegral $ port
+        let ipv = CUChar . fromIntegral $ case (ipv6) of
+                      True -> 1
+                      False -> 0
+        c_tox_bootstrap_from_address tox a ipv p k
+        free k
+        free a
+
+foreign import ccall "tox_get_address"
+    c_tox_get_address :: Ptr Tox -> Ptr CUChar -> IO ()
+
+toxGetAddress :: Ptr Tox -> IO (String)
+toxGetAddress tox = do
+        addrPtr <- (mallocArray 64 :: IO (Ptr CUChar))
+        c_tox_get_address tox addrPtr
+        addrArray <- peekArray 64 addrPtr
+        free addrPtr
+        putStrLn $ show $ addrArray
+        return (map (toEnum . fromIntegral) addrArray)
